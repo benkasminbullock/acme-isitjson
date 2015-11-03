ea5fb4e8861b96e469a9c75f758e825a8b8008b5
Ben Bullock <benkasminbullock@gmail.com>
2015-11-03 15:18:17 +0900

    Initial commit of acme-isitjson

---
+++ lib/Acme/IsItJSON.pm
@@ -0,0 +1,27 @@
+=encoding UTF-8
+
+=head1 NAME
+
+Acme::IsItJSON - abstract here.
+
+=head1 SYNOPSIS
+
+    use Acme::IsItJSON;
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=cut
+package Acme::IsItJSON;
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT_OK = qw//;
+%EXPORT_TAGS = (
+    all => \@EXPORT_OK,
+);
+use warnings;
+use strict;
+use Carp;
+our $VERSION = 0.01;
+1;
---
+++ t/acme-isitjson.t
@@ -0,0 +1,11 @@
+# This is a test for module Acme::IsItJSON.
+
+use warnings;
+use strict;
+use Test::More;
+use Acme::IsItJSON;
+
+done_testing ();
+# Local variables:
+# mode: perl
+# End:
